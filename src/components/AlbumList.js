2ea318f5c81def460ddecf45e3301f38b7160c32
MadurangaKM <madurangakodithuwakku@gmail.com>
2018-07-25 12:53:34 +0530

    loading

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, ActivityIndicator, View, StyleSheet, Text } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
+
 export default class AlbumList extends Component {
-    state = { albums: [] }
+   
+  constructor(props) {
+    super(props);
+    this.state = { albums: [], isLoading: true };
+  }
+    
+
     componentWillMount() {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-            .then(responce => this.setState({ albums: responce.data }));
+            .then(responce => this.setState({ albums: responce.data, isLoading: false }));
     }
 
     renderAlbums() {
+      if (this.state.isLoading) {
+        return (
+        <View style={styles.containerStyle}> 
+            <ActivityIndicator size="large" color="#0000ff" />
+            <Text style={styles.containerStyle}>Loading Data</Text>
+        </View>
+        );
+      }  
         return this.state.albums.map(album => 
-        <AlbumDetail key={album.title} album={album} />);
+         
+            <AlbumDetail key={album.title} album={album} />
+          
+        );
     }
 
   render() {
@@ -23,5 +41,14 @@ export default class AlbumList extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+    containerStyle: {
+        paddingTop: 20,
+        textAlign: 'center',
+    },
+
+   
+});
 module.exports = AlbumList;
 /* eslint-disable eol-last */
